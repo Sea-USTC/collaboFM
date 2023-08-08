b9d62ae1ce92aa67771b77851b32ca5497f06363
lisiyi <ustclsy@gmail.com>
2023-08-08 13:48:19 +0800

    lisiyi: sync some scripts

---
+++ exp_script/20caltech/run.sh
@@ -0,0 +1,2 @@
+cd /mnt/workspace/colla_group/collaboFM
+python main.py --cfg /mnt/workspace/colla_group/exp_script/20caltech/config.yaml
---
+++ exp_script/vitcaltech/run.sh
@@ -0,0 +1,2 @@
+cd /mnt/workspace/colla_group/collaboFM
+python /mnt/workspace/colla_group/collaboFM/main.py --cfg /mnt/workspace/colla_group/exp_script/vitcaltech/config.yaml
